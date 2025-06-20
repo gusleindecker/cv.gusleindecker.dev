08318cba349e9c20289a37a448bc88e32fb074b5
Gustavo Leindecker <723481+gusleindecker@users.noreply.github.com>
2025-06-20 18:52:36 -0300

    Adding metadata to the HTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: `${process.env.NEXT_PUBLIC_FULL_NAME || "Your Name"} - Resume`,
+  description:
+    "Senior Frontend Engineer | Vue.js | React | TypeScript | Nuxt | Next.js | Tailwindcss | Node.js",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
